d0d97c9f3259a80793fd8fff0ac2161f582d9727
Slincker5 <hipersocial08@gmail.com>
2024-07-14 00:57:08 -0600

    fix bug

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,8 @@ import axios from "axios";
 import { toast } from "vue3-toastify";
 import "vue3-toastify/dist/index.css";
 import Cargando from "@/components/Cargando.vue";
+import EscanerVainilla from "@/components/EscanerVainilla.vue";
+
 
 const cargando = ref(false)
 const fileInput = ref(null);
@@ -30,7 +32,7 @@ async function uploadFile() {
       formData
     );
 
-    if(data.status === "OK"){
+    if (data.status === "OK") {
       toast.success(data.message, {
         theme: "colored",
         autoClose: 1500,
@@ -38,7 +40,7 @@ async function uploadFile() {
         transition: toast.TRANSITIONS.ZOOM,
       });
     }
-    
+
     // Maneja la respuesta de éxito aquí
   } catch (error) {
     console.error("Error al subir el archivo:", error);
@@ -52,27 +54,43 @@ const escaneados = ref([])
 const noEscaneados = ref([])
 
 
-const getListScan = async() => {
-  try{
+const getListScan = async () => {
+  try {
 
     const { data } = await axios.get('https://steven.multimarcas.app/api/document/list')
     escaneados.value = data
-  }catch(error){
+  } catch (error) {
     console.log(error)
   }
 }
 
-const getList = async() => {
-  try{
+const getList = async () => {
+  try {
     const { data } = await axios.get('https://steven.multimarcas.app/api/document/productos-restantes')
     noEscaneados.value = data
-  }catch(error){
+  } catch (error) {
     console.log(error)
   }
 }
 
 getList()
 getListScan()
+
+const startScannerNew = async (barcode) => {
+  try {
+    const dataPackage = {
+      articulo: barcode
+    };
+    const { data } = await axios.post('https://steven.multimarcas.app/api/document/escanear', dataPackage, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    alert(data.message)
+  } catch (error) {
+    console.log(error);
+  }
+};
 </script>
 
 <template>
@@ -82,34 +100,37 @@ getListScan()
       class="w-full py-2 px-3 center font-medium text-gray-700 border-2 border-dashed border-[#555] block uppercase"
       @click="abrirModal">cargar inventario</button>
 
-      <Transition>
-        <div class="fixed top-0 left-0 w-full h-full bg-black/40 flex items-center justify-center" v-if="modal">
-          <div class="p-4 bg-white w-[90%]">
-            <h2 class="font-medium mb-4 flex items-center justify-between">CARGAR INVENTARIO <button class="font-extrabold"
-                @click="cerrarModal">X</button></h2>
-            <form @submit.prevent="uploadFile">
-              <input type="file" name="xlsx_file" ref="fileInput">
-              <input type="submit" class="bg-blue-500 block w-full mt-4 px-3 py-2 text-white font-medium"
-                value="CARGAR DOCUMENTO" </form>
-          </div>
+    <Transition>
+      <div class="fixed top-0 left-0 w-full h-full bg-black/40 flex items-center justify-center" v-if="modal">
+        <div class="p-4 bg-white w-[90%]">
+          <h2 class="font-medium mb-4 flex items-center justify-between">CARGAR INVENTARIO <button
+              class="font-extrabold" @click="cerrarModal">X</button></h2>
+          <form @submit.prevent="uploadFile">
+            <input type="file" name="xlsx_file" ref="fileInput">
+            <input type="submit" class="bg-blue-500 block w-full mt-4 px-3 py-2 text-white font-medium"
+              value="CARGAR DOCUMENTO" </form>
         </div>
-      </Transition>
-
-      <div class="py-4 font-medium flex items-center justify-between" v-if="noEscaneados">
-<router-link to="/">NO ESCANEADOS <span class="font-medium">({{ noEscaneados.length }})</span></router-link>
-<router-link to="/escaneados">
-  ESCANEADOS <span class="font-medium">({{ escaneados.length }})</span>
-</router-link>
-
       </div>
-      <div class="md:grid md:grid-cols-3 gap-4">
+    </Transition>
+    <EscanerVainilla @startScannerNew="startScannerNew">
+    </EscanerVainilla>
+    <div class="py-4 font-medium flex items-center justify-between" v-if="noEscaneados">
+      <router-link to="/">NO ESCANEADOS <span class="font-medium">({{ noEscaneados.length }})</span></router-link>
+      <router-link to="/escaneados">
+        ESCANEADOS <span class="font-medium">({{ escaneados.length }})</span>
+      </router-link>
+
+    </div>
+    <div class="md:grid md:grid-cols-3 gap-4">
 
       <div v-if="noEscaneados" v-for="articulo in noEscaneados" class="p-4 border border-solid border-[#ddd] mb-4">
         <div class="truncate font-medium">{{ articulo.descripcion }}</div>
         <div class="py-2 text-sm">BARRA: {{ articulo.articulo }}</div>
-        <div class="flex items-center justify-between"><span class="text-sm">Costo: {{ articulo.costo }}</span>  <span class="text-sm">Precio: {{ articulo.precio }}</span>  <span class="text-sm">Antiguedad: {{ articulo.antiguedad }}</span></div> 
+        <div class="flex items-center justify-between"><span class="text-sm">Costo: {{ articulo.costo }}</span> <span
+            class="text-sm">Precio: {{ articulo.precio }}</span> <span class="text-sm">Antiguedad: {{
+              articulo.antiguedad }}</span></div>
       </div>
-      </div>
+    </div>
 
   </div>
 
