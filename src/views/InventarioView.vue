bd1ca27281bf010b106491ca32c1ca2e034cdf81
Slincker5 <hipersocial08@gmail.com>
2024-07-24 06:03:07 -0600

    pwa

--- src/views/InventarioView.vue
+++ src/views/InventarioView.vue
@@ -189,7 +189,7 @@ const startScannerNew = async (barcode) => {
 
     </div>
     <div class="p-4 py-0 font-medium flex items-center justify-between" v-if="noEscaneados">
-      <router-link to="/">NO ESCANEADOS <span class="font-medium">({{ noEscaneados.length }})</span></router-link>
+      <router-link to="/inventario">NO ESCANEADOS <span class="font-medium">({{ noEscaneados.length }})</span></router-link>
       <router-link to="/escaneados">
         ESCANEADOS <span class="font-medium">({{ escaneados.length }})</span>
       </router-link>
